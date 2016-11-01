1924eef8cd6998195b8b50c5e9e4154b0d70ee16
jspspike <jspspike@gmail.com>
2016-11-01 16:10:08 -0500

    Added better RPM correction

--- TeamCode/src/main/java/org/firstinspires/ftc/quadx/Teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/quadx/Teleop/Teleop.java
@@ -1,35 +1,4 @@
-/*
-Copyright (c) 2016 Robert Atkinson
 
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.quadx.Teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -57,11 +26,13 @@ public class Teleop extends LinearOpMode {
     double oldFly = 0.0;
     double flyRPM = 0;
     int rpmValCount = 0;
-    double[] rpmVals = new double[100];
+    double[] rpmVals = new double[POLL_RATE];
     double rpmAvg;
     double desiredRPM = 0;
 
     private ElapsedTime runtime = new ElapsedTime();
+
+    public static final int POLL_RATE = 40;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -176,7 +147,7 @@ public class Teleop extends LinearOpMode {
             }
 
             else if (gamepad2.b) {
-                desiredRPM = 1350;
+                desiredRPM = 1315;
             }
 
             else if (gamepad2.x) {
@@ -203,13 +174,13 @@ public class Teleop extends LinearOpMode {
 
             oldFly = Math.abs(fly.getCurrentPosition());
 
-            if (rpmValCount > 99) {
+            if (rpmValCount > POLL_RATE - 1) {
                 rpmAvg = 0;
                 for (int i = 0; i < rpmVals.length; i++) {
                     rpmAvg += rpmVals[i];
                 }
 
-                rpmAvg /= 100;
+                rpmAvg /= POLL_RATE;
                 rpmValCount = 0;
             }
 
@@ -225,11 +196,11 @@ public class Teleop extends LinearOpMode {
             }
 
             if (rpmAvg - desiredRPM <= -100 && flyPow >= -1) {
-                flyPow -= .01;
+                flyPow -= Math.abs(rpmAvg - desiredRPM)/200000;
             }
 
             else if (rpmAvg - desiredRPM >= 100 && flyPow <= 0) {
-                flyPow += .01;
+                flyPow += Math.abs(rpmAvg - desiredRPM)/200000;
             }
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
