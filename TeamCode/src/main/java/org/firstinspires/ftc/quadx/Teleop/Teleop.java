1b7c4875075d78e6d3eafd1298518c3e732040b2
jspspike <jspspike@gmail.com>
2016-11-03 16:29:43 -0500

    RedPusher not commited

--- TeamCode/src/main/java/org/firstinspires/ftc/quadx/Teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/quadx/Teleop/Teleop.java
@@ -1,96 +1,91 @@
-/*
-Copyright (c) 2016 Robert Atkinson
 
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.quadx.Teleop;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
-@TeleOp(name="Template: Linear OpMode", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+
+@TeleOp(name="TeleOp", group="Teleop")
 public class Teleop extends LinearOpMode {
 
+    DcMotor fly;
+    //DcMotor manip;
     DcMotor motorBL;
     DcMotor motorBR;
     DcMotor motorFL;
     DcMotor motorFR;
-    DcMotor manip;
-    DcMotor fly;
 
+    Servo door;
+    Servo buttonP;
+
+    double flyPow = 0.0;
+    double oldFly = 0.0;
+    double flyRPM = 0;
+    int rpmValCount = 0;
+    double[] rpmVals = new double[POLL_RATE];
+    double rpmAvg;
+    double desiredRPM = 0;
 
     private ElapsedTime runtime = new ElapsedTime();
+
+    public static final int POLL_RATE = 40;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
+        fly = hardwareMap.dcMotor.get("fly");
+        //manip = hardwareMap.dcMotor.get("manip");
         motorBL = hardwareMap.dcMotor.get("motorBL");
         motorBR = hardwareMap.dcMotor.get("motorBR");
         motorFL = hardwareMap.dcMotor.get("motorFL");
         motorFR = hardwareMap.dcMotor.get("motorFR");
-        manip = hardwareMap.dcMotor.get("manip");
-        fly = hardwareMap.dcMotor.get("fly");
 
+        door = hardwareMap.servo.get("door");
+        buttonP = hardwareMap.servo.get("buttonP");
+
+        fly.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //manip.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        manip.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        fly.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        door.setPosition(.2);
+        buttonP.setPosition(.5);
 
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
 
+        /*Runnable flyWheel = new Runnable() {
+            @Override
+            public void run() {
+                while (opModeIsActive()) {
+                    if (gamepad1.a){
+                        fly.setPower(1);
+                    }
+                    else if (gamepad1.b) {
+                        fly.setPower(-1);
+                    }
+                    else {
+                        fly.setPower(0);
+                    }
+                    try {
+                        idle();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
 
         Runnable manipulator = new Runnable() {
             @Override
@@ -115,44 +110,26 @@ public class Teleop extends LinearOpMode {
                 }
             }
         };
-        Runnable flyWheel = new Runnable() {
-            @Override
-            public void run() {
-                while (opModeIsActive()) {
-                    if (gamepad1.a){
-                        fly.setPower(1);
-                    }
-                    else if (gamepad1.b) {
-                        fly.setPower(-1);
-                    }
-                    else {
-                        fly.setPower(0);
-                    }
-                    try {
-                        idle();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
 
 
         Thread manipThread = new Thread(manipulator);
         manipThread.start();
 
+
         Thread flyWheelThread = new Thread(flyWheel);
         flyWheelThread.start();
 
+        */
+
+        resetStartTime();
+
         while (opModeIsActive()) {
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
 
             if (Math.abs(gamepad1.left_stick_y) > .05 || Math.abs(gamepad1.right_stick_y) > .05) {
-                motorBL.setPower(-gamepad1.left_stick_y);
-                motorBR.setPower(gamepad1.right_stick_y);
-                motorFL.setPower(-gamepad1.left_stick_y);
-                motorFR.setPower(gamepad1.right_stick_y);
+                motorBL.setPower(gamepad1.left_stick_y);
+                motorBR.setPower(-gamepad1.right_stick_y);
+                motorFL.setPower(gamepad1.left_stick_y);
+                motorFR.setPower(-gamepad1.right_stick_y);
             } else {
                 motorBL.setPower(0);
                 motorBR.setPower(0);
@@ -160,10 +137,85 @@ public class Teleop extends LinearOpMode {
                 motorFR.setPower(0);
             }
 
+            if (gamepad2.left_bumper)
+                door.setPosition(.6);
+            else if (gamepad2.right_bumper)
+                door.setPosition(.2);
+
+            if (gamepad2.a) {
+                desiredRPM = 1600;
+            }
+
+            else if (gamepad2.b) {
+                desiredRPM = 1315;
+            }
+
+            else if (gamepad2.x) {
+                desiredRPM = 1450;
+            }
+
+            else if (gamepad2.y) {
+                desiredRPM = 0;
+            }
+
+            if (gamepad2.dpad_left) {
+                desiredRPM -= 10;
+                Thread.sleep(200);
+            }
+
+            else if (gamepad2.dpad_right) {
+                desiredRPM += 10;
+                Thread.sleep(200);
+            }
+
+            fly.setPower(flyPow);
+
+            flyRPM = (Math.abs(fly.getCurrentPosition()) - oldFly) / getRuntime();
+
+            oldFly = Math.abs(fly.getCurrentPosition());
+
+            if (rpmValCount > POLL_RATE - 1) {
+                rpmAvg = 0;
+                for (int i = 0; i < rpmVals.length; i++) {
+                    rpmAvg += rpmVals[i];
+                }
+
+                rpmAvg /= POLL_RATE;
+                rpmValCount = 0;
+            }
+
+
+            else {
+                rpmVals[rpmValCount] = flyRPM;
+                rpmValCount++;
+            }
+
+
+            if (desiredRPM <= 0) {
+                flyPow = 0;
+            }
+
+            if (rpmAvg - desiredRPM <= -100 && flyPow >= -1) {
+                flyPow -= Math.abs(rpmAvg - desiredRPM)/200000;
+            }
+
+            else if (rpmAvg - desiredRPM >= 100 && flyPow <= 0) {
+                flyPow += Math.abs(rpmAvg - desiredRPM)/200000;
+            }
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Flypow", flyPow * -1);
+//            telemetry.addData("Fly Position", fly.getCurrentPosition());
+//            telemetry.addData("oldFly", oldFly);
+            telemetry.addData("DesiredRPM", desiredRPM);
+            telemetry.addData("FlyRPM", rpmAvg);
+            telemetry.update();
+
+            resetStartTime();
+            Thread.sleep(10);
             idle();
         }
-
-        }
-
     }
+
+}
 
