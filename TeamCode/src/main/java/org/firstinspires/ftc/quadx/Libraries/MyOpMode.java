3cfb84289bb06bd7c4571d126b2feb0ecc8ec46d
jspspike <jspspike@gmail.com>
2017-04-10 16:14:35 -0500

    Fixed modifing time failsafe

--- TeamCode/src/main/java/org/firstinspires/ftc/quadx/Libraries/MyOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/quadx/Libraries/MyOpMode.java
@@ -1379,7 +1379,7 @@ public abstract class MyOpMode extends LinearOpMode {
                 if (time2.seconds() > .25) {
                     if (getEncoderAverage() - prevEncoder < 100 && failSafe) {
                         arcTurnPID(-.65, -20, 1000);
-                        tim += 1;
+                        tim += 1000;
                     }
                     time2.reset();
                     prevEncoder = getEncoderAverage();
@@ -1397,7 +1397,7 @@ public abstract class MyOpMode extends LinearOpMode {
                 if (time2.seconds() > .25) {
                     if (getEncoderAverage() - prevEncoder < 100 && failSafe) {
                         arcTurnPID(.65, 20, 1000);
-                        tim += 1;
+                        tim += 1000;
                     }
                     time2.reset();
                     prevEncoder = getEncoderAverage();
